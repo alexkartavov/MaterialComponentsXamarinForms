4ef56bec0acdc72f4d466f83ed0db62ef63d4d55
Alex Kartavov <alexanderk@outlook.com>
2018-06-26 12:16:13 -0400

    initial commit

---
+++ MaterialComponentsXamarinForms/ButtonsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MaterialComponentsXamarinForms
+{
+    public partial class ButtonsPage : ContentPage
+    {
+        public ButtonsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MaterialComponentsXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MaterialComponentsXamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnPgButtonClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ButtonsPage());
+        }
+
+        //void Handle_Clicked(object sender, System.EventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+}
